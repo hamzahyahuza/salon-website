5c671dd79b90fc1dfb89ca79f9cb35635c2c5f0d
hamzahyahuza <hamzahyehuza@gmail.com>
2025-12-28 23:14:27 +0000

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('index');
+  res.render('index'); // views/index.ejs
 });
 
 export default router;
